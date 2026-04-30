a81a2ad96132c09bdf3947bd1c408b2e1ccfb507
Jakub <41429302+jjezewski@users.noreply.github.com>
2026-04-30 17:01:59 +0200

    Dodanie nazwiska

--- Prezentacja_io.c
+++ Prezentacja_io.c
@@ -2,7 +2,8 @@
 
 int main() {
     printf("Dzisiaj zaprezentujemy jak dziala GIT oraz GITHUB.\n");
-    printf("Magdalena Sawicka\n")
+    printf("Magdalena Sawicka\n");
+    printf("Jakub Jeżewski\n");
 
     return 0;
 }
